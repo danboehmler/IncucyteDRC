4b2574d6e8dc47ae66195eedf531b39cd829e0ed
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2016-03-22 12:32:20 +0000

    Version 0.1.4: Added fitIndividualSplines and plotIncucyteDRCSet functions
    Fit a spline through the growth curve data and generate a plot of the
    curves

---
+++ R/fitIndividualSplines.R
@@ -0,0 +1,47 @@
+#' fitIndividualSplines
+#'
+#' Function to fit splines to the growth curve data in an IncucyteDRCSet object
+#'
+#' @param idrc_set IncucyteDRCSet object
+#'
+#' @return IncucyteDRCSet object
+#' @export
+#'
+#' @examples
+#' test_pm <- importPlatemapXML(system.file(file='extdata/example.PlateMap', package='IncucyteDRC'))
+#' test_data <- importIncucyteData(system.file(file='extdata/example_data.txt', package='IncucyteDRC'), metric='pc')
+#'
+#' test_list <- splitIncucyteDRCPlateData(test_pm, test_data, group_columns='growthcondition')
+#'
+#' str(test_list)
+#'
+#' test_splines <- fitIndividualSplines(test_list[[2]])
+#'
+fitIndividualSplines <- function(idrc_set) {
+
+    #combine the platemap and data
+    data <- idrc_set$platemap %>% dplyr::inner_join(idrc_set$platedata$data, by='wellid')
+
+    #fit the splines
+    fitted_models <- data %>%
+        dplyr::group_by(wellid, sampleid, conc) %>%
+        dplyr::do(gc_model=loess (value ~ elapsed  , ., span=0.3, control=loess.control(surface='direct')))
+
+    #establish the data range
+    data_range <- data$elapsed
+
+    #generate the fitted data for plotting
+    fitted_data <- fitted_models %>%
+        dplyr::do(data.frame(value=predict(.$gc_model, data_range), elapsed=data_range, wellid=.$wellid, sampleid=.$sampleid, conc=.$conc, stringsAsFactors=FALSE)) %>%
+        dplyr::ungroup() %>%
+        as.data.frame()
+
+    #construct the output object
+    output <- idrc_set
+    output$fitted_data <- fitted_data
+    output$fitted_models <- fitted_models
+
+    return(output)
+
+
+}
---
+++ R/plotIncucyteDRCSet.R
@@ -0,0 +1,33 @@
+#' plotIncucyteDRCSet
+#'
+#' Plot the growth curve data from an IncucyteDRCSet object
+#'
+#' @param idrc_set IncucyteDRCSet object
+#'
+#' @return ggplot object
+#' @export
+#' @import ggplot2
+#'
+#' @examples
+#' test_pm <- importPlatemapXML(system.file(file='extdata/example.PlateMap', package='IncucyteDRC'))
+#' test_data <- importIncucyteData(system.file(file='extdata/example_data.txt', package='IncucyteDRC'), metric='pc')
+#'
+#' test_list <- splitIncucyteDRCPlateData(test_pm, test_data, group_columns='growthcondition')
+#'
+#' str(test_list)
+#'
+#' test_splines <- fitIndividualSplines(test_list[[2]])
+#' plotIncucyteDRCSet(test_splines)
+#'
+plotIncucyteDRCSet <- function(idrc_set) {
+
+    #combine the platemap and data
+    data <- idrc_set$platemap %>% dplyr::inner_join(idrc_set$platedata$data, by='wellid')
+
+    ggplot(data, aes(x=elapsed, y=value, colour=as.factor(round(conc, 3)))) +
+        geom_point() +
+        geom_line(data=idrc_set$fitted_data, aes(group=wellid)) +
+        scale_colour_discrete(name='conc') +
+        facet_wrap(~sampleid) + theme_bw()
+
+}
