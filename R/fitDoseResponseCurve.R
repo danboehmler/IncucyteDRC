2c4e68a9cbb7777d803831d1a5f7f68719a2a070
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2016-03-25 11:44:57 +0000

    Version 0.2.5: Added calculateEC50 function to split up fitDoseResponseCurve into 2

--- R/fitDoseResponseCurve.R
+++ R/fitDoseResponseCurve.R
@@ -22,6 +22,8 @@
 #' plotIncucyteDRCSet(test_drc)
 #' exportDRCtoDataFrame(test_drc)
 #' test_drc <- fitDoseResponseCurve(test_drc)
+#' test_drc <- calculateEC50(test_drc)
+#' exportEC50Data(test_drc)
 #'
 fitDoseResponseCurve <- function(idrc_set, include_control=FALSE) {
 
@@ -41,35 +43,9 @@ fitDoseResponseCurve <- function(idrc_set, include_control=FALSE) {
         dplyr::group_by(sampleid) %>%
         dplyr::do(drc_model=drc_fit(.))
 
-    #extract parameters from models filtering out NULLs
-    drc_ec50 <- drc_models %>%
-        dplyr::filter(!is.null(drc_model)) %>%
-        dplyr::mutate(Tm = drc::ED(drc_model,50, display=F)[1],
-               SE = drc::ED(drc_model,50, display=F)[2],
-               #R2 <- dr.calc.r2(dr),
-               b = coef(drc_model)[1],
-               c = coef(drc_model)[2],
-               d = coef(drc_model)[3],
-               e = coef(drc_model)[4]) %>%
-        dplyr::select(-drc_model) %>%
-        dplyr::ungroup()
-
-    #deal with null models
-    drc_ec50_nulls <- drc_models %>%
-        dplyr::filter(is.null(drc_model)) %>%
-        dplyr::mutate(Tm=NA, SE=NA, b=NA, c=NA, d=NA, e=NA) %>%
-        dplyr::select(-drc_model) %>%
-        dplyr::ungroup()
-
-    #combine outputs
-    drc_ec50 <- drc_ec50 %>%
-        dplyr::bind_rows(drc_ec50_nulls) %>%
-        as.data.frame()
-
     #sort out output
     output <- idrc_set
     output$drc_models <- drc_models
-    output$drc_ec50 <- drc_ec50
 
     return(output)
 
